bb9c2da7a08d87ad7cae5663733782e235ba6362
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2012-02-02 13:57:58 +0200

    tabs to spaces

--- tests/test.js
+++ tests/test.js
@@ -6,18 +6,18 @@ function sleep(ms) {
 }
 
 jobs.pushJob({ run: function() { console.log('job.run: job#0'); sleep(1000); return 10; }},
-			 function(result) { console.log('job#0 finished: ' + result); } );
+             function(result) { console.log('job#0 finished: ' + result); } );
 jobs.pushJob({ run: function() { console.log('job.run: job#1'); return 20; }},
-			 function(result) { console.log('job#1 finished: ' + result); } );
+             function(result) { console.log('job#1 finished: ' + result); } );
 jobs.pushJob({ run: function() { console.log('job.run: job#2'); return 30; }},
-			 function(result) { console.log('job#2 finished: ' + result); } );
+             function(result) { console.log('job#2 finished: ' + result); } );
 jobs.pushJob({ run: function() { console.log('job.run: job#3'); return 40; }},
-			 function(result) { console.log('job#3 finished: ' + result); } );
+             function(result) { console.log('job#3 finished: ' + result); } );
 jobs.pushJob({ run: function() { console.log('job.run: job#4'); return 50; }},
-			 function(result) { console.log('job#4 finished: ' + result); } );
+             function(result) { console.log('job#4 finished: ' + result); } );
 jobs.pushJob({ run: function() { console.log('job.run: job#5'); return 60; }},
-			 function(result) { console.log('job#5 finished: ' + result); } );
+             function(result) { console.log('job#5 finished: ' + result); } );
 jobs.pushJob({ run: function() { console.log('job.run: job#6'); return 70; }},
-			 function(result) { console.log('job#6 finished: ' + result); } );
+             function(result) { console.log('job#6 finished: ' + result); } );
 
 jobs.runNext();
