8093380cbb67810fd77abcf389d05bdf736c5361
Sergey Belov <arikon@yandex-team.ru>
2012-02-03 19:10:18 +0200

    maxWorkers and tests

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,5 @@
-var chunks = require('../lib/parts/chunks.js').newChunks(),
+var maxWorkers = 3,
+    chunks = require('../lib/parts/chunks.js').newChunks(maxWorkers),
     newNode = require('../lib/parts/node.js').newNode;
 
 var a1 = newNode('A1 (root node)', function() { console.log('run(A1)'); return 10 }),
@@ -16,7 +17,37 @@ var a1 = newNode('A1 (root node)', function() { console.log('run(A1)'); return 1
 d.addParent(b2);
 d.addParent(c2);
 
-chunks.fillChunks(a1);
+var aa1 = newNode('AA1 (root node)', function() {
+        console.log('run(AA1)');
+        setTimeout(function() {
+            console.log('done(AA1)');
+            // TODO: need to return 10;
+        }, 2000);
+    }),
+    aa2 = aa1.addChild(newNode('AA2', function() { console.log('run(AA2)'); return 20 })),
+    bb1 = aa2.addChild(newNode('BB1', function() { console.log('run(BB1)'); return 30 })),
+    bb2 = bb1.addChild(newNode('BB2', function() {
+        console.log('run(BB2)');
+        setTimeout(function() {
+            console.log('done(BB2)');
+            // TODO: need to return 40;
+        }, 3000);
+    })),
+    cc1 = aa2.addChild(newNode('CC1', function() { console.log('run(CC1)'); return 50 })),
+    cc2 = cc1.addChild(newNode('CC2', function() { console.log('run(CC2)'); return 60 })),
+
+    root = newNode('Pseudo', function() {
+        console.log('run(Pseudo)');
+        setTimeout(function() {
+            console.log('done(Pseudo)');
+            // TODO: mark job as done
+        }, 2000);
+    });
+
+root.addChild(a1);
+root.addChild(aa1);
+
+chunks.fillChunks(root);
 console.log('=========\nINITIAL CHUNKS\n---------');
 console.log(chunks.toString());
 console.log('=========\nRUN JOBS\n---------');
