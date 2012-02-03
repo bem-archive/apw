f9a1c55874e3b4a986c887198176aaa9ed54715a
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2012-02-03 02:15:17 +0200

    refactored

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,23 @@
-var jobs = require('../lib/parts/jobs.js').newJobs();
+var chunks = require('../lib/parts/chunks.js').newChunks(),
+    newNode = require('../lib/parts/node.js').newNode;
 
-function sleep(ms) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + ms);
-}
+var a1 = newNode('A1 (root node)', function() { console.log('run(A1)') }),
+    a2 = a1.addChild(newNode('A2', function() { console.log('run(A2)') })),
+    b1 = a2.addChild(newNode('B1', function() { console.log('run(B1)') })),
+    b2 = b1.addChild(newNode('B2', function() { console.log('run(B2)') })),
+    c1 = a2.addChild(newNode('C1', function() { console.log('run(C1)') })),
+    c2 = c1.addChild(newNode('C2', function() { console.log('run(C2)') })),
+    d = newNode('D', function() { console.log('run(D)') }),
+    e1 = d.addChild(newNode('E1', function() { console.log('run(E1)') })),
+    e2 = e1.addChild(newNode('E2', function() { console.log('run(E2)') })),
+    f1 = d.addChild(newNode('F1', function() { console.log('run(F1)') })),
+    f2 = f1.addChild(newNode('F2', function() { console.log('run(F2)') }));
 
-jobs.pushJob({ run: function() { console.log('job.run: job#0'); sleep(1000); return 10; }},
-             function(result) { console.log('job#0 finished: ' + result); } );
-jobs.pushJob({ run: function() { console.log('job.run: job#1'); return 20; }},
-             function(result) { console.log('job#1 finished: ' + result); } );
-jobs.pushJob({ run: function() { console.log('job.run: job#2'); return 30; }},
-             function(result) { console.log('job#2 finished: ' + result); } );
-jobs.pushJob({ run: function() { console.log('job.run: job#3'); return 40; }},
-             function(result) { console.log('job#3 finished: ' + result); } );
-jobs.pushJob({ run: function() { console.log('job.run: job#4'); return 50; }},
-             function(result) { console.log('job#4 finished: ' + result); } );
-jobs.pushJob({ run: function() { console.log('job.run: job#5'); return 60; }},
-             function(result) { console.log('job#5 finished: ' + result); } );
-jobs.pushJob({ run: function() { console.log('job.run: job#6'); return 70; }},
-             function(result) { console.log('job#6 finished: ' + result); } );
+d.addParent(b2);
+d.addParent(c2);
 
-jobs.runNext();
+chunks.extractChunks(a1);
+console.log('=========\nINITIAL CHUNKS\n---------');
+console.log(chunks.toString());
+console.log('=========\nRUN JOBS\n---------');
+chunks.pushJobs();
