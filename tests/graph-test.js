ebf8534d2ebbb8b0db1ee377a28ca7ee2e6a2597
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2012-03-02 13:09:44 +0200

    vows-graph initial

---
+++ tests/graph-test.js
@@ -0,0 +1,56 @@
+var VOWS = require('vows'),
+	assert = require('assert'),
+    suite = VOWS.describe('Graph'),
+    CORE = require('../lib/core.js');
+
+function getEmptyGraph() {
+    var graph = new CORE.Graph();
+    graph.setNode({
+        getId: function() {
+            return 'all';
+        },
+        run: function() {
+            console.error("** Nothing to be done for '%s'", this.getId());
+        }
+    });
+    return graph;
+}
+
+suite
+    .addBatch({
+        'Simple set/get': {
+            topic: function() {
+                var graph = getEmptyGraph();
+                
+                graph.setNode('A', { run: 'testA' });
+                graph.setNode('B', { run: 'testB' }, 'A');
+                
+                return graph;
+            },
+            'getNode() A': function(graph) {
+                var node = graph.getNode('A');
+                
+                assert.equal(node.id, 'A');
+                assert.equal(node.node.run, 'testA');
+            },
+            'getNode() default': function(graph) {
+                var node = graph.getNode('XXX');
+                
+                assert.equal(node.id, 'XXX');
+            },
+            'getChildrenIds() A': function(graph) {
+                var children = graph.getChildrenIds('A');
+                
+                assert.lengthOf(children, 1);
+                assert.equal(children[0], 'B');
+            },
+            'getParentsIds() B': function(graph) {
+                var parents = graph.getParentsIds('B');
+                
+                assert.lengthOf(parents, 1);
+                assert.equal(parents[0], 'A');
+            }
+        }
+    });
+
+suite.export(module);
