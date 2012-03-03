76d235e7ffe501005322296e0626734c33119eb8
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2012-03-04 01:12:59 +0200

    vows pool: Run plan A

--- tests/pool-test.js
+++ tests/pool-test.js
@@ -10,9 +10,11 @@ function getEmptyGraph() {
 
 function getSimpleGraph(state) {
     var graph = getEmptyGraph();
+
+    graph.setNode('0A', { run: function() { state.push('0A') } });
     
-    graph.setNode('A', { run: function() { state.push('A') } });
-    graph.setNode('B', { run: function() { state.push('B') } }, 'A');
+    graph.setNode('1A', { run: function() { state.push('1A') } });
+    graph.setNode('1B', { run: function() { state.push('1B') } }, '1A');
     
     return graph;
 }
@@ -34,12 +36,28 @@ function getRunner(graph, force, method, maxWorkers) {
 
 suite
     .addBatch({
-        'Chain A -> B': {
+        'Run plan: A': {
             topic: function() {
                 var _this = this,
                     state = [];
                 Q.when(
-                    getRunner(getSimpleGraph(state), true).process('A'),
+                    getRunner(getSimpleGraph(state), true).process('0A'),
+                    function(value) { _this.callback(null, state) },
+                    function(error) { _this.callback(error, null) }
+                )
+            },
+            'correct run': function(error, state) {
+                assert.isNull(error);
+                assert.lengthOf(state, 1);
+                assert.equal(state[0], '0A');
+            }            
+        },
+        'Run plan: A -> B': {
+            topic: function() {
+                var _this = this,
+                    state = [];
+                Q.when(
+                    getRunner(getSimpleGraph(state), true).process('1A'),
                     function(value) { _this.callback(null, state) },
                     function(error) { _this.callback(error, null) }
                 )
@@ -47,8 +65,8 @@ suite
             'correct run order': function(error, state) {
                 assert.isNull(error);
                 assert.lengthOf(state, 2);
-                assert.equal(state[0], 'B');
-                assert.equal(state[1], 'A');
+                assert.equal(state[0], '1B');
+                assert.equal(state[1], '1A');
             }
         }
     });
